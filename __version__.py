b14cc21be328bf2c7c6e48424ce7c0f2c31d7849
lkacz <lkacz1@gmail.com>
2025-12-29 00:05:27 +0100

    v3.0.4 - Priorities dialog improvements
    - Rename 'My Priorities for Today' to 'My Priorities' (general, multi-day)
    - Change planned time from minutes to hours
    - Update progress tracking to use hours
    - Session time logging still in minutes but converts to hours when saved
    
    All 155 tests passing.

--- __version__.py
+++ __version__.py
@@ -1,6 +1,6 @@
 """Version information for Personal Freedom."""
 
-__version__ = "3.0.3"
+__version__ = "3.0.4"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 
 # Release information
