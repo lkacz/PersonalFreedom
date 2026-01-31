80f2b05b3e385faf89ca87d50cee0e19dac5f35b
lkacz <lkacz1@gmail.com>
2026-01-31 19:12:12 +0100

    Fix City Tab graphics refresh on state changes (v6.0.30)

--- __version__.py
+++ __version__.py
@@ -1,6 +1,6 @@
 """Version information for Personal Liberty."""
 
-__version__ = "6.0.29"
+__version__ = "6.0.30"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 
 # Release information
